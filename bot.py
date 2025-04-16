fa079cd3a988de1acc19c26cc94a0a13d34c3a47
MBALASTA12 <nk.marlonbalasta@gmail.com>
2025-04-16 19:29:11 +0800

    INITIAL

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ bot = Bot(token=os.environ["BOT_API_TOKEN"])
 @app.route("/ipn", methods=["POST"])
 def ipn():
     ipn_data = request.form.to_dict()
-    verification_url = "https://ipnpb.paypal.com/cgi-bin/webscr"  # LIVE endpoint
+    verification_url = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr"  # LIVE endpoint
     verification_data = {
         "cmd": "_notify-validate",
         **ipn_data
